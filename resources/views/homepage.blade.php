7bcdd8d3b0cb951d269e8fc9af48307c254dd62a
Aquarius <binhvm.kimson@gmail.com>
2019-06-24 23:52:58 +0700

    update layout homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,12 +2,40 @@
 
 @section('content')
     <!-- Page Content -->
-	<div class="container">
+    <div class="container">
 
-	@if(session('thongbao'))
-		<div class="alert alert-danger">{{session('thongbao')}}</div>
-	@endif
-	
+        <div class="space20"></div>
+
+        <div class="row">
+            <div class="col-md-8">
+	            <div class="panel panel-default">
+	            	<div class="panel-body">
+	            		<!-- item -->
+					    <div class="row-item row">
+		                	<h4>
+		                		<a href="#">Category</a> | 	
+		                		<small><a href="#"><i>subtitle</i></a>/</small>
+		                	</h4>
+		                	<div class="col-md-12">
+		                		<img src="#" alt="">
+		                	</div>
+		                </div>
+					</div>
+	            </div>
+        	</div>
+        	<div class="col-md-4">
+        		<div class="panel panel-default">
+        			<div class="panel-body">
+	        			<a href="detail.html">
+							<h4>
+								<span class="glyphicon glyphicon-list-alt"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							</h4>
+						</a>
+        			</div>
+        		</div>
+        	</div>
+        </div>
+        <!-- /.row -->
     </div>
     <!-- end Page Content -->
 @endsection
